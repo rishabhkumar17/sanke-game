63671891a29daf173270f697f80c5d9f7fe5a584
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-08-08 19:37:11 +0530

    fn draw_rectangle

--- src/draw.rs
+++ src/draw.rs
@@ -13,8 +13,33 @@ pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d ) {
 
     rectangle(
         color,
-        [gui_x, gui_y, BLOCK_SIZEm BLOCK_SIZE],
-        con,
+        [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
+        con.transform,
+        g,
+    );
+}
+
+pub fn draw_rectangle(
+    color: Color,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32, 
+    con: &Context,
+    g: &mut G2d 
+) {
+    let x: to_coord(x);
+    let y: to_coord(y);
+
+    rectangle(
+        color,
+        [
+            x,
+            y,
+            BLOCK_SIZE * (width as f64),
+            BLOCK_SIZE * (height as f64), 
+        ],
+        con.transform,
         g,
     );
 }
