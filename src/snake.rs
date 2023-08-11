ab25dc822ae6605d7ba7e7d5ba6daf5f4c0c98c8
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-08-11 20:34:59 +0530

    default snake

--- src/snake.rs
+++ src/snake.rs
@@ -34,3 +34,29 @@ pub struct Snake {
     body: LinkedList<Block>,
     tail: Option<Block>,
 }
+
+impl Snake {
+    pub fn new(x: i32, y: i32) -> Snake {
+        let mut body: LinkedList<Block> = LinkedList::new();
+        body.push_back(Block { x: x + 2, y });
+        body.push_back(Block { x: x + 1, y });
+        body.push_back(Block { x, y });
+
+        Snake {
+            direction: Direction::Right,
+            body,
+            tail: None,
+        }
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        for block in &self.body {
+            draw_block(SNAKE_COLOR, block.x, block.y, con, g);
+        }
+    }
+
+    pub fn head_position(&self) -> (i32, i32) {
+        let head_block = self.body.front().unwrap();
+        (head_block.x, head_block.y)
+    }
+}
